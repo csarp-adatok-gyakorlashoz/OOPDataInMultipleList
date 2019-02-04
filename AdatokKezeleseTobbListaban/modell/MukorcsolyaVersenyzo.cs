f990802726c5d6b7cae088fc16cf3daadb365f79
Csaba Gyuris <gycsaba@vasvari.hu>
2019-02-04 15:18:27 +0100

    Mukorcsolya projekt és Versenyzo és Mukorcsolya versenyzo osztályok

---
+++ AdatokKezeleseTobbListaban/modell/MukorcsolyaVersenyzo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdatokKezeleseTobbListaban.modell
+{
+    abstract class MukorcsolyaVersenyzo : Versenyzo
+    {
+        protected double tehcnikai;
+        protected double komponens;
+        protected  int levonas;
+
+        public MukorcsolyaVersenyzo(string nev, string orszag, double technikai, double komponens, int levonas)
+            :base(nev,orszag)
+        {
+            this.tehcnikai = technikai;
+            this.komponens = komponens;
+            this.levonas = levonas;
+        }
+    }
+}
---
+++ AdatokKezeleseTobbListaban/modell/Versenyzo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdatokKezeleseTobbListaban.modell
+{
+    abstract class Versenyzo
+    {
+        protected string nev;
+        protected string orszag;
+        
+        public Versenyzo(string nev, string orszag)
+        {
+            this.nev = nev;
+            this.orszag = orszag;
+        }
+
+        public string getNev()
+        {
+            return nev;
+        }
+        public string getOrszag()
+        {
+            return orszag;
+        }
+
+        public void setNev(string nev)
+        {
+            this.nev = nev;
+        }
+
+        public void setOrszag(string orszag)
+        {
+            this.orszag = orszag;
+        }
+
+    }
+}
