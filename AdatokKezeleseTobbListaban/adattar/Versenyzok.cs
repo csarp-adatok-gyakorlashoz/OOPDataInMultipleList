c19b31a8e36a89aeb50b11b5bef806b4beefc354
Csaba Gyuris <gycsaba@vasvari.hu>
2019-02-07 14:26:56 +0100

    Adatfelvitel - hibás adaok, javítani kell

---
+++ AdatokKezeleseTobbListaban/adattar/Versenyzok.cs
@@ -0,0 +1,15 @@
+﻿using AdatokKezeleseTobbListaban.modell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdatokKezeleseTobbListaban.adattar
+{
+    class Versenyzok
+    {
+        List<MukorcsolyaVersenyzo> dontbeSzereploVersenyzok;
+        List<MukorcsolyaVersenyzo> rovidporgrambaSzereploVersenyzok;
+    }
+}
--- AdatokKezeleseTobbListaban/modell/MukorcsolyaVersenyzo.cs
+++ AdatokKezeleseTobbListaban/modell/MukorcsolyaVersenyzo.cs
@@ -6,18 +6,45 @@ using System.Threading.Tasks;
 
 namespace AdatokKezeleseTobbListaban.modell
 {
-    abstract class MukorcsolyaVersenyzo : Versenyzo
+    class MukorcsolyaVersenyzo : Versenyzo
     {
-        protected double tehcnikai;
+        protected double technikai;
         protected double komponens;
         protected  int levonas;
 
         public MukorcsolyaVersenyzo(string nev, string orszag, double technikai, double komponens, int levonas)
             :base(nev,orszag)
         {
-            this.tehcnikai = technikai;
+            this.technikai = technikai;
             this.komponens = komponens;
             this.levonas = levonas;
         }
+
+        public double getTechnikai()
+        {
+            return technikai;
+        }
+        public double getKomponens()
+        {
+            return komponens;
+        }
+        public int getLevonas()
+        {
+            return levonas;
+        }
+
+        public void setTechnikai(double technikai)
+        {
+            this.technikai = technikai;
+        }
+        public void setKomponens(double komponens)
+        {
+            this.komponens = komponens;
+        }
+        public void setLevonas(int levonas)
+        {
+            this.levonas = levonas;
+        }
     }
+
 }
